e4f60f6277e286158d3059fe56f4536d37c8ddf8
Mert Yılmaz <m3rtylmz@gmail.com>
2022-08-19 15:07:38 +0200

    TemplateDesignPattern

---
+++ DesignPatterns/WebApp.Template/UserCards/PrimeUserCardTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace WebApp.Template.UserCards
+{
+    public class PrimeUserCardTemplate : UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            var sb = new StringBuilder();        
+            sb.Append("<a href='#' class='card-link'>Send Message</a>");
+            sb.Append("<a href='#' class='card-link'>Profile Detail</a>");
+            return sb.ToString();
+
+        }
+
+        protected override string SetPicture()
+        {
+            return $"<img class='card-img-top' src='{AppUser.PictureUrl}'>";
+        }
+    }
+}
